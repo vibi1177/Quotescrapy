cb7be016fb439af04e44ba3da4d8dac053fb9fc2
vibi1177 <30286373+vibi1177@users.noreply.github.com>
2018-07-16 16:14:44 +0200

    Update quotes.py

--- quotes/spiders/quotes.py
+++ quotes/spiders/quotes.py
@@ -18,7 +18,7 @@ class QuotesSpider(scrapy.Spider):
                 'text': quote.css('span.text::text').extract_first(),
                 'author': quote.xpath('span/small/text()').extract_first(),
                 'tag': quote.css('a.tag::text').extract(),
-                'detail' : scrapy.Request(absolute_url, callback=self.parse_attr)
+                'detail' : scrapy.Request(absolute_url, callback=self.parse_attr),
             }
             
         next_page = response.css('li.next a::attr("href")').extract_first()
@@ -27,4 +27,4 @@ class QuotesSpider(scrapy.Spider):
    def parse_attr(self, response):
         item = QuotesItem()
         item["born"] = response.css('span.author-born-location::text').extract_first()
-        return item
+        yield item
