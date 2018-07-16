4fc9434da8a2c23bf6d6cc5cf4d5aa3687d1d32d
vibi1177 <30286373+vibi1177@users.noreply.github.com>
2018-07-16 16:28:29 +0200

    Update quotes.py

--- quotes/spiders/quotes.py
+++ quotes/spiders/quotes.py
@@ -11,20 +11,18 @@ class QuotesSpider(scrapy.Spider):
     
     def parse(self, response):
         for quote in response.css('div.quote'):
-            links = quote.xpath('span/a/@href').extract()
-            for link in links:
-                absolute_url = self.BASE_URL + link
-            yield {
-                'text': quote.css('span.text::text').extract_first(),
-                'author': quote.xpath('span/small/text()').extract_first(),
-                'tag': quote.css('a.tag::text').extract(),
-                'detail' : scrapy.Request(absolute_url, callback=self.parse_attr),
-            }
-            
+            yield  scrapy.Request(quote, callback=self.parse_attr),
         next_page = response.css('li.next a::attr("href")').extract_first()
         if next_page is not None:
             yield response.follow(next_page, self.parse)
+            
    def parse_attr(self, response):
         item = QuotesItem()
-        item["born"] = response.css('span.author-born-location::text').extract_first()
+            links = response.xpath('span/a/@href').extract()
+            for link in links:
+                absolute_url = self.BASE_URL + link
+                item["text"] = response.css('span.text::text').extract_first(),
+                item["author"] = response.xpath('span/small/text()').extract_first(),
+                item["tag"] = response.css('a.tag::text').extract(),
+                item["born"] = response.css('span.author-born-location::text').extract_first()
         yield item
