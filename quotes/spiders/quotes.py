cc522678ba92ad42bcbce670284dd4674e9394f5
vibi1177 <30286373+vibi1177@users.noreply.github.com>
2018-07-16 16:42:40 +0200

    Update quotes.py

--- quotes/spiders/quotes.py
+++ quotes/spiders/quotes.py
@@ -9,17 +9,17 @@ class QuotesSpider(scrapy.Spider):
     
     def parse(self, response):
         for quote in response.css('div.quote'):
-            yield  scrapy.Request(quote, callback=self.parse_attr)
+            link = response.xpath('span/a/@href').extract()
+            absolute_url = self.BASE_URL + link
+            yield  scrapy.Request(response.urljoin(link), callback=self.parse_attr)
         next_page = response.css('li.next a::attr("href")').extract_first()
         if next_page is not None:
             yield response.follow(next_page, self.parse)
             
    def parse_attr(self, response):
         item = QuotesItem()
-        links = response.xpath('span/a/@href').extract()
-        BASE_URL = 'http://quotes.toscrape.com/'
-        for link in links:
-            absolute_url = self.BASE_URL + link
+
+ 
             item["text"] = response.css('span.text::text').extract_first()
             item["author"] = response.xpath('span/small/text()').extract_first()
             item["tag"] = response.css('a.tag::text').extract()
