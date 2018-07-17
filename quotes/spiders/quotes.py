ebb56519510fe410793aa1e8863f318b6299ce4e
vibi1177 <30286373+vibi1177@users.noreply.github.com>
2018-07-17 10:44:30 +0200

    Update quotes.py

--- quotes/spiders/quotes.py
+++ quotes/spiders/quotes.py
@@ -9,7 +9,7 @@ class QuotesSpider(scrapy.Spider):
     
     def parse(self, response):
         for quote in response.css('div.quote'):
-            link = quote.xpath('span/a/@href').extract_first()
+            #link = quote.xpath('span/a/@href').extract_first()
             yield scrapy.Request(response.urljoin(link), callback=self.parse_attr)
         next_page = response.css('li.next a::attr("href")').extract_first()
         if next_page:
